bf69f240b8ba7f0b5cccf3a6a4ac77885f9e2465
rajender38 <rajenderameelpur@gmail.com>
2020-10-19 18:55:53 +1000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import ReactDOM from 'react-dom';
+import './App.css';
+import SearchPage from './components/searchPage';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux';
+import { render } from 'react-dom';
+import Header from './components/common/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = configureStore();
+
+render(
+  <ReduxProvider store={store}>
+    <Header />
+    <SearchPage />
+  </ReduxProvider>,
+  document.getElementById('app')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
